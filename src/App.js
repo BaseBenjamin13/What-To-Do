f5203cf669b2ef98598235b60386f4282190bde9
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-12-23 21:22:47 -0500

    could do form init no function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/Header';
+import CouldDo from './components/CouldDo';
 
 function App() {
   return (
     <div className="App">
         <Header />
+
+        <CouldDo />
+
     </div>
   );
 }
---
+++ src/components/CouldDo.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function CouldDo() {
+  return (
+    <div>
+         <form >
+            <label htmlFor="coulddo">Could Do: </label>
+            <input type="text" id="coulddo"></input>
+            <br></br>
+
+            <label htmlFor="coulddo">Could Do: </label>
+            <input type="text" id="coulddo"></input>
+            <br></br>
+
+            <label htmlFor="coulddo">Could Do: </label>
+            <input type="text" id="coulddo"></input>
+            <br></br>
+
+            <label htmlFor="coulddo">Could Do: </label>
+            <input type="text" id="coulddo"></input>
+            <br></br>
+
+        </form>
+    </div>
+  )
+}
+
+export default CouldDo
